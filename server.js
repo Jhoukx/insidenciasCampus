bf913042c3777204adb8fbe94b4fc682eca41ddf
John <jegonzalezesparragoza@gmail.com>
2023-07-16 21:42:43 -0500

    feat: :sparkles: Server levantado

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const appServer = express();
+
+const config = JSON.parse(process.env.SERVER);
+
+appServer.listen(config, () => {
+    console.log(`Server is running on http://${config.host}:${config.port}`);
+})
+
